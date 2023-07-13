6b98d27e4fe4b7d55a50244aee68879de0a14ff8
sashaus <sashaus359@gmail.com>
2023-07-13 17:03:20 +0300

    Attach added

--- src/test/java/com/sashaus/tests/RemoteBaseTest.java
+++ src/test/java/com/sashaus/tests/RemoteBaseTest.java
@@ -2,8 +2,10 @@ package com.sashaus.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import com.sashaus.helpers.Attach;
 import com.sashaus.pages.AutomationPracticeFormPage;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -32,6 +34,14 @@ public class RemoteBaseTest {
     @BeforeEach
     void addListener() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttach() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
 
