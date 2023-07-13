98dc2dfe85d5487b894a16e89bdc6ef060fce09b
sashaus <sashaus359@gmail.com>
2023-07-13 16:11:40 +0300

    BaseTest beforeAll upd

--- src/test/java/com/sashaus/tests/BaseTest.java
+++ src/test/java/com/sashaus/tests/BaseTest.java
@@ -11,5 +11,6 @@ public class BaseTest {
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
     }
 }
